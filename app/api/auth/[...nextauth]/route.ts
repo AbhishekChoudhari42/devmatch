22924c6729cc7e1e7baf233b8ba0b8a9e888562a
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2023-08-15 09:40:32 +0530

    authoprions type added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
-import NextAuth from "next-auth"
+import NextAuth,{NextAuthOptions} from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-const handler = NextAuth({
-    // Configure one or more authentication providers
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string,
-      }),
-      // ...add more providers here
-    ],
-})
-
+const authOptions : NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+}
+const handler = NextAuth(authOptions)
 export {handler as GET, handler as POST}
