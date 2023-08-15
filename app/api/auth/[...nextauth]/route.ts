8c710301460bc194e233cc060e9ddd5e8fb36906
AbhishekChoudhari42 <choudhariabhishek93@gmail.com>
2023-08-15 09:28:27 +0530

    authoptions error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
+import NextAuth,{NextAuthOptions} from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
+export const authOptions : NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
